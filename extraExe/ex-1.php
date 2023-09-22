c51aca3aad53a0b0748b9baf7a09e995a91b67ee
ansonsarturs <ansonsarturs@gmail.com>
2023-09-22 09:34:21 +0300

    rr

--- extraExe/ex-1.php
+++ extraExe/ex-1.php
@@ -1,12 +1,10 @@
 <?php
-// Prompt the user to enter elements separated by commas
-$userElements = readline("Enter your elements separated by commas (e.g., rock, paper, scissors): ");
-$elements = array_map('trim', explode(',', $userElements));
 
-if (count($elements) < 2) {
-    echo "You must enter at least two elements.\n";
-    exit();
-}
+$rock = "rock";
+$paper  = "paper";
+$scissors = "scissors";
+
+$elements = [$rock, $paper, $scissors];
 
 $userSelection = readline('Enter your element (' . implode(', ', $elements) . "): ");
 
@@ -15,21 +13,18 @@ if (!in_array(strtolower($userSelection), $elements)) {
     exit();
 }
 
-// Generate the computer's choice
 $computerSelection = $elements[array_rand($elements)];
 
 echo "Computer selected: $computerSelection\n";
 
-// Determine the winner
 if ($userSelection === $computerSelection) {
     echo "It's a tie!\n";
+} elseif (
+    ($userSelection === $rock && $computerSelection === $scissors) ||
+    ($userSelection === $scissors && $computerSelection === $paper) ||
+    ($userSelection === $paper && $computerSelection === $rock)
+) {
+    echo "You win!\n";
 } else {
-    $userIndex = array_search($userSelection, $elements);
-    $computerIndex = array_search($computerSelection, $elements);
-
-    if (($userIndex + 1) % count($elements) === $computerIndex) {
-        echo "You win!\n";
-    } else {
-        echo "Computer wins!\n";
-    }
+    echo "Computer wins!\n";
 }
