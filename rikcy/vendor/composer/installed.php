e52764a20028f4fd6d761ec474fe78d8135011ea
ansonsarturs <ansonsarturs@gmail.com>
2023-10-24 12:15:03 +0300

    24.10.2023

---
+++ rikcy/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'artursansons/rikcy',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '21dc1d2b0e265e5e7059050b7b91b9936cda033c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'artursansons/rikcy' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '21dc1d2b0e265e5e7059050b7b91b9936cda033c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
